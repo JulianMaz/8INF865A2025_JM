9043c7686a176680b60500cd705d203c8f6ee91f
Julian Mazerat <julian@2a02-8440-510d-6ce5-c235-32ff-fe62-ec89.rev.sfr.net>
2025-09-21 15:54:09 -0400

    Étape7

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -27,18 +27,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Text(
         text = message,
         fontSize = 100.sp,
         lineHeight = 116.sp,
         )
+    Text(
+        text = from,
+        fontSize = 36.sp
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Sam!")
+        GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
     }
 }
