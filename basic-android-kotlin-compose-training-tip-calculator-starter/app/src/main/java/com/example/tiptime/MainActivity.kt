dbb49111955d0432595a36ab66dd32123b4fe232
Julian Mazerat <julian@2a02-8440-510d-6ce5-c235-32ff-fe62-ec89.rev.sfr.net>
2025-10-20 11:34:08 -0400

    Étape8

--- basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -28,6 +28,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -45,6 +46,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.KeyboardType
 
 
 var amountInput: MutableState<String> = mutableStateOf("0")
@@ -112,9 +114,11 @@ fun TipTimeLayoutPreview() {
 fun EditNumberField(modifier: Modifier = Modifier) {
     var amountInput by remember { mutableStateOf("") }
     TextField(
-        value = amountInput,
-        onValueChange = { amountInput = it },
-        modifier = modifier
+        value = amountInput, onValueChange = { amountInput = it },
+        modifier = modifier,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        label = { Text(stringResource(R.string.bill_amount)) }
     )
 }
 
