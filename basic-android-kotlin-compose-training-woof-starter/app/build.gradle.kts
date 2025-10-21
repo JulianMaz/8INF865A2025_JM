c79d48cdb6391f7584f6af81d4f26f7c198efa51
Julian Mazerat <julian@2a02-8440-510d-6ce5-c235-32ff-fe62-ec89.rev.sfr.net>
2025-10-21 09:27:52 -0400

    Étape6

--- basic-android-kotlin-compose-training-woof-starter/app/build.gradle.kts
+++ basic-android-kotlin-compose-training-woof-starter/app/build.gradle.kts
@@ -74,6 +74,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.compose.ui:ui-text:1.9.3")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
